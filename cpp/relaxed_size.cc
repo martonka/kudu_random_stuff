8d96cfd0bbef30a3cd2db16388a44d9dfe75e784
Zoltan Martonka <zmartonka@gmail.com>
2024-11-05 10:58:59 +0100

    relaxed size example

---
+++ cpp/relaxed_size.cc
@@ -0,0 +1,37 @@
+#include <atomic>
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+   int value_;
+   atomic<Node*> next;
+};
+
+struct MyAppendOnlyContainer {
+    atomic<Node*> first{nullptr};
+    atomic<Node*>* last{&first};
+    atomic<size_t> num_elements{0};
+
+
+    // add only called from 1 thread, so no concurrent calls.
+    void add_value(int x) {
+        Node* n = new Node();
+        n->value_ = x;
+        last->store(n, memory_order::memory_order_release);
+        last = &n->next;
+        // std::atomic_signal_fence(std::memory_order_release); <-- this is required
+        num_elements.fetch_add(1, memory_order::memory_order_relaxed);
+    }
+
+    void print() {
+        int size = num_elements;
+        Node* it = first.load(memory_order::memory_order_relaxed);
+        for(int i = 0; i<size; ++i) {
+            cout << it->value_ << std::endl;
+            it = it->next.load(memory_order_relaxed);
+        }
+
+    }
+
+};
