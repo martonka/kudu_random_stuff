4e977cbff875e4b8996a3bd354d1fc52911a428d
Zoltan Martonka <zmartonka@gmail.com>
2024-11-05 11:09:33 +0100

    make relaxed example more readable

--- cpp/relaxed_size.cc
+++ cpp/relaxed_size.cc
@@ -3,35 +3,39 @@
 
 using namespace std;
 
-struct Node {
-   int value_;
-   atomic<Node*> next;
+struct Node
+{
+    int value_;
+    atomic<Node *> next;
 };
 
-struct MyAppendOnlyContainer {
-    atomic<Node*> first{nullptr};
-    atomic<Node*>* last{&first};
+struct MyAppendOnlyContainer
+{
+    atomic<Node *> first{nullptr};
+    atomic<Node *> *last{&first};
     atomic<size_t> num_elements{0};
 
-
     // add only called from 1 thread, so no concurrent calls.
-    void add_value(int x) {
-        Node* n = new Node();
+    void add_value(int x)
+    {
+        Node *n = new Node();
         n->value_ = x;
-        last->store(n, memory_order::memory_order_release);
+        auto l2 = last;
         last = &n->next;
-        // std::atomic_thread_fence(std::memory_order_release); <-- this is required
+        l2->store(n, memory_order::memory_order_release);
+        // std::atomic_thread_fence(std::memory_order_release); // <-- this is required
         num_elements.fetch_add(1, memory_order::memory_order_relaxed);
     }
 
-    void print() {
+    void print()
+    {
         int size = num_elements;
-        Node* it = first.load(memory_order::memory_order_relaxed);
-        for(int i = 0; i<size; ++i) {
+        Node *it = first.load(memory_order::memory_order_relaxed);
+        for (int i = 0; i < size; ++i)
+        {
             cout << it->value_ << std::endl;
             it = it->next.load(memory_order_relaxed);
         }
-
     }
-
 };
+
